3268f5d958644b48657321566e17a1ab7eb10ab0
NicklasTegner <NicklasMCHD@live.dk>
2021-12-20 16:01:52 +0100

    Added tmglr command runner and update command as a proof

--- pytinytex/__init__.py
+++ pytinytex/__init__.py
@@ -1,10 +1,22 @@
+import sys
+import subprocess
 import glob
 import os
 
 from .tinytex_download import download_tinytex
 
 # Global cache
-__tinytex_path = None
+__tinytex_path = getattr(os.environ, "PYTINYTEX_TINYTEX", None)
+
+def update(package="-all"):
+	path = get_tinytex_path()
+	try:
+		code, stdout,stderr = _run_tlmgr_command(["update", package], path, False)
+		return True
+	except RuntimeError:
+		raise
+		return False
+
 
 def get_tinytex_path(base="."):
 	global __tinytex_path
@@ -69,5 +81,52 @@ def _check_file(dir, prefix):
 			if os.path.splitext(s)[0] == prefix and os.path.isfile(os.path.join(dir, s)):
 				return True
 	except FileNotFoundError:
-		raise RuntimeError("Unable to resolve TinyTeX path.")
-	return False
+		raise RuntimeError("Unable to resolve path.")
+		return False
+
+def _get_file(dir, prefix):
+	try:
+		for s in os.listdir(dir):
+			if os.path.splitext(s)[0] == prefix and os.path.isfile(os.path.join(dir, s)):
+				return os.path.join(dir, s)
+	except FileNotFoundError:
+		raise RuntimeError("Unable to find {}.".format(prefix))
+
+def _run_tlmgr_command(args, path, machine_readable=True):
+	if machine_readable:
+		if "--machine-readable" not in args:
+			args.insert(0, "--machine-readable")
+	tlmgr_executable = _get_file(path, "tlmgr")
+	args.insert(0, tlmgr_executable)
+	new_env = os.environ.copy()
+	creation_flag = 0x08000000 if sys.platform == "win32" else 0 # set creation flag to not open TinyTeX in new console on windows
+	p = subprocess.Popen(
+		args,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
+		env=new_env,
+		creationflags=creation_flag)
+	# something else than 'None' indicates that the process already terminated
+	if not (p.returncode is None):
+		raise RuntimeError(
+			'TLMGR died with exitcode "%s" before receiving input: %s' % (p.returncode,
+																		   p.stderr.read())
+		)
+	
+	stdout, stderr = p.communicate()
+	
+	try:
+		stdout = stdout.decode("utf-8")
+	except UnicodeDecodeError:
+		raise RuntimeError("Unable to decode stdout from TinyTeX")
+	
+	try:
+		stderr = stderr.decode("utf-8")
+	except UnicodeDecodeError:
+		raise RuntimeError("Unable to decode stderr from TinyTeX")
+	
+	if stderr == "" and p.returncode == 0:
+		return p.returncode, stdout, stderr
+	else:
+		raise RuntimeError("TLMGR died with the following error:\n{0}".format(stderr.strip()))
+		return p.returncode, stdout, stderr
