d254013a22c7127b3ab5b69cbac216bc66eca49c
JessicaTegner <jessica@jessicategner.com>
2025-02-23 00:45:46 +0100

    fixed ruff errors

--- tests/test_tinytex_commands.py
+++ tests/test_tinytex_commands.py
@@ -1,8 +1,8 @@
 import pytinytex
 
-from .utils import download_tinytex
+from .utils import download_tinytex # noqa: F401
 
-def test_help(download_tinytex):
+def test_help(download_tinytex): # noqa: F811
 	exit_code, output = pytinytex.help()
 	assert exit_code == 0
 	assert "the native TeX Live Manager".lower() in output.lower()
